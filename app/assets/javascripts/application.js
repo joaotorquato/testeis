e0f74431ed1119008c9c18c9101f590857678698
João Torquato <joao.otl@gmail.com>
2016-05-02 14:35:35 -0300

    add and setup gems for the project

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap-sprockets
 //= require turbolinks
+//= require rails.validations
+//= require rails.validations.simple_form
 //= require_tree .
